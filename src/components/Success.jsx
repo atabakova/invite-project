a256389e0789eb8a6d44e6995d1caf12e08b823b
Anastasiia Tabakova <atabakova2016@gmail.com>
2023-11-03 20:57:43 +0000

    2

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Success = ({ count }) => {
   return (
     <div class="success-block">
-      <img src="/assets/success.svg" alt="Success" />
+      <img src="assets/success.svg" alt="Success" />
       <h3>Done!</h3>
       <p>All {count} users received the invintations.</p>
       <button
